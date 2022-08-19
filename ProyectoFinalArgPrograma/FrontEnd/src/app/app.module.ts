dfd064d3d8ce629ccb911320237840ee63efc99d
rehpaloma <palomareh95@gmail.com>
2022-08-19 13:39:36 -0300

    agregue el login

--- ProyectoFinalArgPrograma/FrontEnd/src/app/app.module.ts
+++ ProyectoFinalArgPrograma/FrontEnd/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HysComponent } from './components/hys/hys.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HysComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
